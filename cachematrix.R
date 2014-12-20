f184e07cfd399074cf1218eee86c4142c5cbde04
eswiskey <eswiskey@users.noreply.github.com>
2014-12-20 05:53:58 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,27 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ # holds the cached value or NULL if nothing is cached
+        # initially nothing is cached so set it to NULL
+m<-NULL
+  set<-function(y){
+  x<<-y
+  m<<-NUL
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x=matrix(), ...) {
+    m<-x$getmatrix()
+    if(!is.null(m)){
+      message("retrieving data")
+      return(m)
+    }
         ## Return a matrix that is the inverse of 'x'
+
+matrix<-x$get()
+    m<-solve(matrix, ...)
+    x$setmatrix(m)
+    m
 }
